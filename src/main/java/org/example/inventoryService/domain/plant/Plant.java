de671b9c16c05416206143d01035fb7a7ccaa9a5
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-02-22 15:35:59 +0200

    Update Plant (add constructors; add logics for CRUD of Diseases and Treatment; add logics for GrowthStage; add @Inheritance; add @DiscriminatorColumn)

--- src/main/java/org/example/inventoryService/domain/plant/Plant.java
+++ src/main/java/org/example/inventoryService/domain/plant/Plant.java
@@ -2,9 +2,14 @@ package org.example.inventoryService.domain.plant;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Data
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "plant_type")
+@Getter
+@NoArgsConstructor
 public abstract class Plant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,4 +19,43 @@ public abstract class Plant {
     HealthStatus healthStatus;
     @Embedded
     GrowthStage growthStage;
+
+    protected Plant(Long batchId){
+        this.batchId = batchId;
+        this.healthStatus = new HealthStatus();
+        this.growthStage = new GrowthStage();
+    }
+
+    public void addDisease(Disease disease) {
+        healthStatus.addDisease(disease);
+    }
+
+    public void removeDisease(Disease disease) {
+        healthStatus.removeDisease(disease);
+    }
+
+    public void clearDisease(){
+        healthStatus.clearDiseases();
+    }
+
+    public void addTreatment(Treatment treatment) {
+        healthStatus.addTreatment(treatment);
+    }
+
+    public void removeTreatment(Treatment treatment) {
+        healthStatus.removeTreatment(treatment);
+    }
+
+    public void clearTreatment(){
+        healthStatus.clearTreatments();
+    }
+
+    public void updateGrowth(int age, double height) {
+        this.growthStage.setAge(age);
+        this.growthStage.setHeight(height);
+    }
+
+    public boolean isHealthy() {
+        return healthStatus.isHealthy();
+    }
 }
