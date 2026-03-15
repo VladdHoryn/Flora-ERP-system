86dd7fbd06f22e62b830846bbceeef5621414a77
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-03-15 21:19:12 +0200

    Add DTO validation for production-service

--- production-service/src/main/java/org/example/dto/PlantSearchRequest.java
+++ production-service/src/main/java/org/example/dto/PlantSearchRequest.java
@@ -1,5 +1,8 @@
 package org.example.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.domain.PlantType;
@@ -7,8 +10,11 @@ import org.example.domain.PlantType;
 @Getter
 @Setter
 public class PlantSearchRequest {
-
+    @NotNull(message = "Plant type is required")
     private PlantType plantType;
+    @NotBlank(message = "Plant name is required")
     private String plantName;
+    @NotNull(message = "Plant age is required")
+    @Min(value = 0, message = "Plant age must be non-negative")
     private Integer plantAge;
 }
