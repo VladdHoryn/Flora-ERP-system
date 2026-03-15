950ca6fc81ea7fbd91a0185b8c5c20bdb9c1b16f
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-03-15 18:23:34 +0200

    Add Configuration for communication of inventory-service with production-service

---
+++ inventory-service/src/main/java/org/example/config/ProductionServiceClient.java
@@ -0,0 +1,17 @@
+package org.example.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ProductionServiceClient {
+    private final RestClient restClient;
+
+    @Value("${production-service.production-path}")
+    private String productionUrl;
+}
