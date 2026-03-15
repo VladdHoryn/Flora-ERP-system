e2fe46199f87c7772b6ae412146fa927a8815bf0
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-03-15 17:25:52 +0200

    Add application.properties for inventory-service Add InventoryServiceApplication for running inventory-service

---
+++ production-service/src/main/java/org/example/config/ClientConfig.java
@@ -0,0 +1,10 @@
+package org.example.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class ClientConfig {
+//    @Value("${inventory-service.base-url}")
+//    private String inventoryUrl;
+//}
