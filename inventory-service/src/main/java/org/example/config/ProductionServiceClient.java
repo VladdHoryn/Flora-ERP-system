7ce8121e2f14f1d6cdce931b4006425a96f87878
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-04-01 23:51:45 +0300

    Add @Retry and @CircuitBreaker with Fallback methods for ProductionServiceClient

--- inventory-service/src/main/java/org/example/config/ProductionServiceClient.java
+++ inventory-service/src/main/java/org/example/config/ProductionServiceClient.java
@@ -1,13 +1,18 @@
 package org.example.config;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.domain.inventory.PlantType;
 import org.example.dto.PlantChangeDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,6 +26,8 @@ public class ProductionServiceClient {
     @Value("${production-service.production-path}")
     private String productionUrl;
 
+    @Retry(name = "productionService", fallbackMethod = "findHealthyFallback")
+    @CircuitBreaker(name = "productionService", fallbackMethod = "findHealthyFallback")
     public List<Long> findHealthyPlantIds(
             PlantType plantType,
             String plantName,
@@ -41,10 +48,37 @@ public class ProductionServiceClient {
                 .body(new ParameterizedTypeReference<List<Long>>() {});
     }
 
+    @Retry(name = "productionService", fallbackMethod = "getChangesFallback")
+    @CircuitBreaker(name = "productionService", fallbackMethod = "getChangesFallback")
     public List<PlantChangeDTO> getChanges(LocalDateTime since){
         return restClient.get()
                 .uri(productionUrl + "/production/v1/plants/changes?since=" + since)
                 .retrieve()
                 .body(new ParameterizedTypeReference<List<PlantChangeDTO>>() {});
+    }
+
+    private void findHealthyFallback(PlantType plantType,
+                                     String plantName,
+                                     Integer plantAge,
+                                     Throwable throwable){
+        log.warn("findHealthyFallback triggered for plantType={} plantName={}, plantAge={}, ex={} ",
+                plantType, plantName, plantAge, throwable.toString());
+
+        throw new ResponseStatusException(
+                HttpStatus.I_AM_A_TEAPOT,
+                "production-service unavailable (findHealthyPlants failed)",
+                throwable
+        );
+    }
+
+    private void getChangesFallback(LocalDateTime since, Throwable throwable){
+        log.warn("getChangesFallback triggered for since={}, ex={} ",
+                since, throwable.toString());
+
+        throw new ResponseStatusException(
+                HttpStatus.I_AM_A_TEAPOT,
+                "production-service unavailable (getChanges failed)",
+                throwable
+        );
     }
 }
