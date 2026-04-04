1e46cd634bdc8e90add1a928cf2ed41fc340b0d3
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-04-04 15:11:10 +0300

    Add structure for OutboxEvent
    Create OutboxEventRepository with all required methods

--- inventory-service/src/main/java/org/example/domain/OutboxEvent.java
+++ inventory-service/src/main/java/org/example/domain/OutboxEvent.java
@@ -1,4 +1,68 @@
 package org.example.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "outbox_event")
+@Getter
+@NoArgsConstructor
 public class OutboxEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "event_id", nullable = false, unique = true)
+    private String eventId;
+
+    //для зв’язку події з основною бізнес-операцією
+    @Column(name = "aggregate_type", nullable = false)
+    private String aggregateType;
+
+    @Column(name = "aggregate_id", nullable = false)
+    private String aggregateId;
+
+    @Column(name = "event_type", nullable = false)
+    private String eventType;
+
+    @Lob //Large Object
+    @Column(nullable = false)
+    private String payload;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "sent_at")
+    private LocalDateTime sentAt;
+
+
+    public OutboxEvent(String aggregateType, String aggregateId, String eventType, String payload) {
+        this.eventId = UUID.randomUUID().toString();
+        this.aggregateType = aggregateType;
+        this.aggregateId = aggregateId;
+        this.eventType = eventType;
+        this.payload = payload;
+        this.status = "NEW";
+        this.createdAt = LocalDateTime.now();
+        this.sentAt = LocalDateTime.now();
+    }
+
+    public void markSent() {
+        this.status = "SENT";
+        this.sentAt = LocalDateTime.now();
+    }
+
+    public void markFailed() {
+        this.status = "FAILED";
+        this.sentAt = LocalDateTime.now();
+    }
 }
