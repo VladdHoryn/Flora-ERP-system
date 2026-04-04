52d848cc1f796f7f6a3d2e2cd6b10635f2119ba9
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-04-04 19:43:58 +0300

    Create Consumer for production-service

---
+++ production-service/src/main/java/org/example/application/InventoryChangesConsumer.java
@@ -0,0 +1,29 @@
+package org.example.application;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.infrastructure.messaging.RabbitConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class InventoryChangesConsumer {
+
+    private final ObjectMapper objectMapper;
+    private final ProductionApplicationService productionApplicationService;
+
+    @RabbitListener(queues = RabbitConfig.INVENTORY_CHANGES_QUEUE)
+    public void consume(String message){
+        try{
+
+
+        } catch (Exception e){
+            log.error("Failed to process message {}", message, e);
+            throw new RuntimeException(e);
+        }
+    }
+}
