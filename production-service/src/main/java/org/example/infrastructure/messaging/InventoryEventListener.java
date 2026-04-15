fe04c3f87b1213ba8b071628e02423d6eff615b2
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-04-15 16:59:51 +0300

    Add more Rabbit configuration for inventory changes Add 2 handle methods in InventoryEventListener for production-service(without logics)

--- production-service/src/main/java/org/example/infrastructure/messaging/InventoryEventListener.java
+++ production-service/src/main/java/org/example/infrastructure/messaging/InventoryEventListener.java
@@ -1,4 +1,27 @@
 package org.example.infrastructure.messaging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.application.ProductionApplicationService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class InventoryEventListener {
+
+    private final ProductionApplicationService productionApplicationService;
+    private final ObjectMapper objectMapper;
+
+    @RabbitListener(queues = RabbitConfig.INVENTORY_CHANGED_QUEUE)
+    private void handleInventoryChanged(String message) throws Exception{
+
+    }
+
+    @RabbitListener(queues = RabbitConfig.INVENTORY_CHANGE_FAILED_QUEUE)
+    private void handleInventoryChangeFailed(String message) throws Exception{
+
+    }
 }
--- production-service/src/main/java/org/example/infrastructure/messaging/RabbitConfig.java
+++ production-service/src/main/java/org/example/infrastructure/messaging/RabbitConfig.java
@@ -12,6 +12,14 @@ public class RabbitConfig {
     public static final String PRODUCTION_CHANGES_QUEUE = "production.changes.queue";
     public static final String PRODUCTION_CHANGES_ROUTING_KEY = "production.changes";
 
+    public static final String PLANT_CHANGES_EXCHANGE = "plants.exchange";
+
+    public static final String INVENTORY_CHANGED_QUEUE = "inventory.changes.queue";
+    public static final String INVENTORY_CHANGE_FAILED_QUEUE = "inventory.changes-failed.queue";
+
+    public static final String INVENTORY_CHANGED_ROUTING_KEY = "inventory.changed";
+    public static final String INVENTORY_CHANGE_FAILED_ROUTING_KEY = "inventory.changed-failed";
+
     @Bean
     public TopicExchange productionExchange(){
         return new TopicExchange(PRODUCTION_EXCHANGE);
@@ -28,5 +36,30 @@ public class RabbitConfig {
                 .bind(productionChangesQueue())
                 .to(productionExchange())
                 .with(PRODUCTION_CHANGES_ROUTING_KEY);
+    }
+
+    @Bean
+    public TopicExchange plantsExchange(){
+        return new TopicExchange(PLANT_CHANGES_EXCHANGE);
+    }
+
+    @Bean
+    public Queue inventoryChangedQueue(){return QueueBuilder.durable(INVENTORY_CHANGED_QUEUE).build();}
+
+    @Bean
+    public Queue inventoryChangeFailedQueue(){return QueueBuilder.durable(INVENTORY_CHANGE_FAILED_QUEUE).build();}
+
+    @Bean
+    public Binding inventoryChangedBinding(){
+        return BindingBuilder.bind(inventoryChangedQueue())
+                .to(plantsExchange())
+                .with(INVENTORY_CHANGED_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding inventoryChangeFailedBinding(){
+        return BindingBuilder.bind(inventoryChangeFailedQueue())
+                .to(plantsExchange())
+                .with(INVENTORY_CHANGE_FAILED_ROUTING_KEY);
     }
 }
