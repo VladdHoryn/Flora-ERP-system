f85cf6f1a41e06936a68610eb6be3bada5e5c429
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2026-04-30 12:58:01 +0300

    feat: add partial response in case inventory-service is down

--- api-gateway/src/main/java/org/example/application/SalesCompositionService.java
+++ api-gateway/src/main/java/org/example/application/SalesCompositionService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Service
 @RequiredArgsConstructor
 public class SalesCompositionService {
@@ -21,23 +23,46 @@ public class SalesCompositionService {
     private String inventoryBaseUrl;
 
     public Mono<SalesDetailsResponse> getSalesDetails(Long salesId){
+
         Mono<SalesResponse> salesMono = webClient.get()
                 .uri(salesBaseUrl + "/sales/v1/orders/{id}", salesId)
                 .retrieve()
-                .bodyToMono(SalesResponse.class);
+                .bodyToMono(SalesResponse.class)
+                .onErrorResume(ex -> {
+                    return Mono.empty();
+                });
 
         return salesMono.flatMap(salesResponse -> {
             Mono<ReservationResponse> reservationMono = webClient.get()
                     .uri(inventoryBaseUrl + "/inventory/v1/reservations/{id}/user", salesResponse.getUser().getId())
                     .retrieve()
-                    .bodyToMono(ReservationResponse.class);
+                    .bodyToMono(ReservationResponse.class)
+                    .timeout(Duration.ofSeconds(2))
+                    .onErrorResume(ex -> {
+                        return Mono.empty();
+                    });
 
-            return reservationMono.map(reservation -> SalesDetailsResponse.builder()
-                    .salesId(salesResponse.getId())
-                    .userId(reservation.getUserId())
-                    .status(salesResponse.getStatus())
-                    .reservation(reservation)
-                    .build());
+            return reservationMono.map(reservation -> buildFullResponse(salesResponse, reservation))
+                    .defaultIfEmpty(buildPartialResponse(salesResponse));
         });
+    }
+
+    private SalesDetailsResponse buildFullResponse(SalesResponse sales,
+                                                   ReservationResponse reservation) {
+        return SalesDetailsResponse.builder()
+                .salesId(sales.getId())
+                .userId(reservation.getUserId())
+                .status(sales.getStatus())
+                .reservation(reservation)
+                .build();
+    }
+
+    private SalesDetailsResponse buildPartialResponse(SalesResponse sales) {
+        return SalesDetailsResponse.builder()
+                .salesId(sales.getId())
+                .userId(sales.getUser().getId())
+                .status(sales.getStatus())
+                .reservation(null)
+                .build();
     }
 }
